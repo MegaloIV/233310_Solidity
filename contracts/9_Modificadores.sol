2914d7ec7840f89832e002303260377c8565fa9d
MegaloIV <matiasf.alcalde.lavado@gmail.com>
2026-05-09 15:57:52 -0500

    Commit2

---
+++ contracts/9_Modificadores.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Modificadores {
+    uint256 private fondos = 0;
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner, "No eres el propietario");
+        _;
+    }
+
+    modifier NotCero() {
+        require(fondos > 0, "Fondo en cero");
+        _;
+    }
+
+    function depositarFondos(uint256 monto) public {
+        fondos = fondos + monto; //fondos = += monto;
+    }
+
+    function retirarFondos(uint256 monto) public isOwner NotCero {
+        //require(msg.sender == owner, "No eres el propietario");
+        require(fondos >= monto, "Saldo insuficiente");
+        fondos = fondos - monto; //fondos = -= monto;
+    }
+
+    function limpiarFondo() public isOwner NotCero {
+        //require(msg.sender == owner, "No eres el propietario");
+        fondos = 0;
+    }
+}
