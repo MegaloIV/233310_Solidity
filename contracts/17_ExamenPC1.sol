f13f6a137f60a8bac9ef392589aea5a50054002f
MegaloIV <matiasf.alcalde.lavado@gmail.com>
2026-05-16 19:41:17 -0500

    'Parte 1'

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -1,1 +1,18 @@
-si
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Biblioteca233310 {
+
+    struct Libro {
+        uint256 id;
+        string titulo;
+        uint256 anio;
+    }
+
+    Libro[] public libros;
+
+    constructor() {
+        console.log("Ejecutado por: 233310 - Matias Felipe Alcalde Lavado");
+    }
+}
